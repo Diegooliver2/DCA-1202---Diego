57a71a51049ff14c0cafdcbacc84e67d216d0299
Diegooliver2 <42583983+Diegooliver2@users.noreply.github.com>
2018-08-29 19:31:33 -0300

    Create Questão12.c

---
+++ Questão12.c
@@ -0,0 +1,10 @@
+aloha[2] = value;
+scanf("%f", &aloha); -> Inválido
+aloha = value";
+printf("%f", aloha);
+coisas[4][4] = aloha[3];
+coisas[5] = aloha; -> Inválido
+pf = value;
+pf = aloha;
+
+Obs.: Os não marcados são válidos.
