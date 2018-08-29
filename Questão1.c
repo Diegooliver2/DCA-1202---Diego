83da3578ad240e9e5db2673932a2c7ee090cf2a5
Diegooliver2 <42583983+Diegooliver2@users.noreply.github.com>
2018-08-29 18:07:05 -0300

    Create Questão1.c

---
+++ Questão1.c
@@ -0,0 +1,3 @@
+O uso do ponteiro é um meio de conseguir desempenho no seu programa, pois através dele temos acesso direto a memória do computador,
+em um programa de computador, a maneira mais rápida de acessar e modificar um objeto é acessar diretamente a memória física onde
+o objeto está armazenado. Podemos também com o mesmo fazermos alocação e desalocação de memória gerando uma flexibilização no gerenciamento da memória. 
